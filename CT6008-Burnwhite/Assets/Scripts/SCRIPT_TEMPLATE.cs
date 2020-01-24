78e996203d79ac45100f8e5e11942bbe3f9f862d
Zack <z_raeburn@hotmail.com>
2020-01-24 04:19:00 +0000

    Started development on full screen shader effects
    Added a system for multiple fullscreen custom shader effects. I also started working on a screen space shockwave shader that currently looks pretty good but needs a system that can dynamically add and manage multiple shockwaves.

---
+++ CT6008-Burnwhite/Assets/Scripts/SCRIPT_TEMPLATE.cs
@@ -0,0 +1,35 @@
+﻿//////////////////////////////////////////////////
+// Author: Author name
+// Date created: Date of the files creation
+// Last edit: Date of the last edit
+// Description: Description of the files purpose and a general explanation of how it works
+// Comments: Any comments relating to the file
+//////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCRIPT_TEMPLATE : MonoBehaviour
+{
+    //////////////////////////////////////////////////
+    //// Variables
+
+    // Example variables
+    public int m_publicInt;
+    private float m_privateFloat;
+    [SerializeField] private bool m_serializedPrivateBool;
+
+    //////////////////////////////////////////////////
+    //// Functions
+
+    /// <summary>
+    /// A template function with comments
+    /// </summary>
+    /// <param name="a_input">An integard input</param>
+    /// <returns>The input</returns>
+    public int MyFunction(int a_input)
+    {
+        return a_input;
+    }
+}
