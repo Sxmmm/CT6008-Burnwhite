ba2e567649d57ba2736e9fe3004d62222c6230e5
Sam Baker <09sam@live.co.uk>
2020-01-31 18:16:27 +0000

    Move Controls, zoom in, simple follower

---
+++ CT6008-Burnwhite/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+
+    private void Update() {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(float f_damage) {
+        health -= f_damage * Time.deltaTime;
+    }
+}
